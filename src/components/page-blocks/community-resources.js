14422b1e587d8b700baec79a3ef104eb7ef36b82
xaralis <filip.varecha@fragaria.cz>
2021-01-18 14:58:42 +0100

    feat: even more unified community visual style

--- src/components/page-blocks/community-resources.js
+++ src/components/page-blocks/community-resources.js
@@ -5,42 +5,44 @@ const CommunityResources = ({ resources, site, location, ...props }) => {
   return (
     <section
       {...props}
-      className="v-community-resources-listing content-block content-block--narrow content-block--shift-mobile"
+      className="content-block content-block--narrow content-block--shift-mobile"
     >
-      {resources.map(({ frontmatter, html }) => {
-        const isExternalLink = !frontmatter.link.startsWith("/")
+      <div className="v-community-resources-listing">
+        {resources.map(({ frontmatter, html }) => {
+          const isExternalLink = !frontmatter.link.startsWith("/")
 
-        return (
-          <article
-            key={frontmatter.link}
-            className="v-community-resources-listing__item"
-          >
-            <h2>{frontmatter.title}</h2>
-            {isExternalLink && (
-              <a
-                href={frontmatter.link}
-                className="v-community-resources-listing-link anchor anchor--default"
-              >
-                {frontmatter.linkTitle}
-              </a>
-            )}
-            {!isExternalLink && (
-              <Link
-                style={{ boxShadow: `none` }}
-                to={frontmatter.link}
-                className="v-community-resources-listing-link anchor anchor--default"
-              >
-                {frontmatter.linkTitle}
-              </Link>
-            )}
-            <div
-              dangerouslySetInnerHTML={{
-                __html: html,
-              }}
-            />
-          </article>
-        )
-      })}
+          return (
+            <article
+              key={frontmatter.link}
+              className="v-community-resources-listing__item"
+            >
+              <h2>{frontmatter.title}</h2>
+              {isExternalLink && (
+                <a
+                  href={frontmatter.link}
+                  className="v-community-resources-listing-link anchor anchor--default"
+                >
+                  {frontmatter.linkTitle}
+                </a>
+              )}
+              {!isExternalLink && (
+                <Link
+                  style={{ boxShadow: `none` }}
+                  to={frontmatter.link}
+                  className="v-community-resources-listing-link anchor anchor--default"
+                >
+                  {frontmatter.linkTitle}
+                </Link>
+              )}
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: html,
+                }}
+              />
+            </article>
+          )
+        })}
+      </div>
     </section>
   )
 }
