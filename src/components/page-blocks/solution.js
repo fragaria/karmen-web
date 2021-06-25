23c1fa1c64a5dc19e520a7ffeaa9bb6f1aceeed6
Adam David <adam.david@fragaria.cz>
2021-06-25 14:54:49 +0200

    feat(homepage): changes solution block into new design with video * adds video player component * adds video file * adds one new icon into fragaria icon pack

--- src/components/page-blocks/solution.js
+++ src/components/page-blocks/solution.js
@@ -1,10 +1,9 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { FormattedMessage } from "react-intl"
-import { CarouselProvider, DotGroup, Slider, Slide } from "pure-react-carousel"
-import "pure-react-carousel/dist/react-carousel.es.css"
-
-import { BackgroundImage } from "components/image"
+import Video from "../../assets/video/meet-karmen-cs.mp4"
+import VideoPoster from "../../assets/img/video-poster.jpg";
+import VideoPlayer from "../video";
 
 const SolutionBlock = props => {
   const data = useStaticQuery(graphql`
@@ -14,14 +13,7 @@ const SolutionBlock = props => {
       ) {
         ...fluidImage1920_noblur
       }
-      smartphoneAndPill: file(relativePath: { eq: "smartphone-and-pill.png" }) {
-        ...fluidImage1024_traced
-      }
-      smartphoneAndPillMobile: file(
-        relativePath: { eq: "pill-w-cloud-mobile.png" }
-      ) {
-        ...fluidImage600_traced
-      }
+
       site {
         siteMetadata {
           siteUrl
@@ -38,152 +30,26 @@ const SolutionBlock = props => {
       <div className="solution" itemType="http://schema.org/Product" itemScope>
         <div className="content-block sitenav__anchorpush">
           <span className="sitenav__anchor" id="meet"></span>
-          <meta itemProp="name" content="Karmen" />
-          <div itemProp="offers" itemType="http://schema.org/Offer" itemScope>
-            <meta itemProp="price" content="130" />
-            <meta itemProp="priceCurrency" content="EUR" />
-            <meta
-              itemProp="availability"
-              content="https://schema.org/InStock"
+          <h1 className="solution__headline">
+            <FormattedMessage
+              id="solution-block.meet_karmen"
+              defaultMessage="Meet Karmen"
             />
-            <meta
-              itemProp="itemCondition"
-              content="https://schema.org/NewCondition"
+          </h1>
+          <h2 className="solution__sub" itemProp="description">
+            <FormattedMessage
+              id="solution-block.claim_1"
+              defaultMessage="Karmen lets you manage your 3D printers remotely."
             />
-            <div
-              itemProp="seller"
-              itemType="http://schema.org/Organization"
-              itemScope
-            >
-              <meta
-                itemProp="name"
-                content={data.site.siteMetadata.company.officialName}
-              />
-            </div>
-          </div>
-          <link
-            itemProp="image"
-            href={`${data.site.siteMetadata.siteUrl}${data.smartphoneAndPillMetadata.childImageSharp.fluid.src}`}
-          />
-          <BackgroundImage
-            file={data.smartphoneAndPill}
-            className="solution__inner"
-            style={{ backgroundPosition: "right 80px", backgroundSize: "44%" }}
-          >
-            <div className="solution__desc">
-              <h1 className="solution__headline">
-                <FormattedMessage
-                  id="solution-block.meet_karmen"
-                  defaultMessage="Meet Karmen"
-                />
-              </h1>
-              <h2 className="solution__sub" itemProp="description">
-                <FormattedMessage
-                  id="solution-block.claim_1"
-                  defaultMessage="Karmen lets you manage your 3D printers remotely."
-                />
-                <br />
-                <FormattedMessage
-                  id="solution-block.claim_2"
-                  defaultMessage="Get rid of SD cards once and for all."
-                />
-              </h2>
-            </div>
-            <BackgroundImage
-              file={data.smartphoneAndPillMobile}
-              style={{
-                backgroundPosition: "left top",
-                backgroundSize: "600px 373px",
-              }}
-              className="solution__mobileimage"
+            <br />
+            <FormattedMessage
+              id="solution-block.claim_2"
+              defaultMessage="Get rid of SD cards once and for all."
             />
-
-            <CarouselProvider
-              naturalSlideWidth={100}
-              naturalSlideHeight={100}
-              interval={4000}
-              isPlaying={true}
-              totalSlides={5}
-              className="solution__carousel"
-            >
-              <Slider>
-                <Slide index={0}>
-                  <h3>
-                    <FormattedMessage
-                      id="solution-block.slide_1_title"
-                      defaultMessage="Your print jobs. Centralized."
-                    />
-                  </h3>
-                  <p>
-                    <FormattedMessage
-                      id="solution-block.slide_1_content"
-                      defaultMessage="It doesn’t matter how many 3D printers you have or where they’re located. Karmen lets you manage your printers on a computer or smartphone. Start, pause and stop print jobs, change the order in the queue or redirect to another device."
-                    />
-                  </p>
-                </Slide>
-                <Slide index={1}>
-                  <h3>
-                    <FormattedMessage
-                      id="solution-block.slide_2_title"
-                      defaultMessage="Right there with you on the road"
-                    />
-                  </h3>
-                  <p>
-                    <FormattedMessage
-                      id="solution-block.slide_2_content"
-                      defaultMessage="It’s easy to send new print jobs over the internet and assign them to printers according to workload or filament colour."
-                    />
-                  </p>
-                </Slide>
-                <Slide index={2}>
-                  <h3>
-                    <FormattedMessage
-                      id="solution-block.slide_3_title"
-                      defaultMessage="Video from print jobs in real time"
-                    />
-                  </h3>
-                  <p>
-                    <FormattedMessage
-                      id="solution-block.slide_3_content"
-                      defaultMessage="Monitor print job progress in real time. Check job status visually anytime and show print progress to colleagues and clients."
-                    />
-                  </p>
-                </Slide>
-                <Slide index={3}>
-                  <h3>
-                    <FormattedMessage
-                      id="solution-block.slide_4_title"
-                      defaultMessage="Easy team management"
-                    />
-                  </h3>
-                  <p>
-                    <FormattedMessage
-                      id="solution-block.slide_4_content"
-                      defaultMessage="Create teams for all your printers. Members can manage their devices remotely, while support has a complete overview of device usage."
-                    />
-                  </p>
-                </Slide>
-                <Slide index={4}>
-                  <h3>
-                    <FormattedMessage
-                      id="solution-block.slide_5_title"
-                      defaultMessage="Tested by the toughest users"
-                    />
-                  </h3>
-                  <p>
-                    <FormattedMessage
-                      id="solution-block.slide_5_content"
-                      defaultMessage="Karmen has proven itself everywhere there’s a need to rapidly and effectively innovate, such as in healthcare and technology companies."
-                    />
-                  </p>
-                </Slide>
-              </Slider>
-
-              <div className="solution__carousel-controls">
-                <DotGroup />
-              </div>
-            </CarouselProvider>
-          </BackgroundImage>
+          </h2>
+        </div>
+        <div className="content-block">
+          <VideoPlayer video={Video} poster={VideoPoster}/>
         </div>
       </div>
     </section>
---
+++ src/components/video.js
@@ -0,0 +1,85 @@
+import React, { useState, useRef, useEffect } from "react"
+
+const VideoPlayer = (props) => {
+  const [playing, setPlaying] = useState(false);
+  const [paused, setPaused] = useState(false);
+  const [showControls, setShowControls] = useState(false);
+  const vidRef = useRef(null);
+  let timer = null;
+
+  useEffect(() => {
+    let video = vidRef;
+    video.current.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      video.current.removeEventListener("mousemove", handleMouseMove);
+      if (timer) clearTimeout(timer);
+    };
+  })
+
+  function playToggle() {
+    setPlaying(!playing);
+    if(!playing) {
+      setPaused(false);
+      vidRef.current.play();
+    } else {
+      setPaused(true);
+      vidRef.current.pause();
+    }
+  }
+  function stop() {
+    setPlaying(false);
+    vidRef.current.currentTime = 0;
+  }
+
+  function replay() {
+    setPlaying(true);
+    setPaused(false);
+    vidRef.current.currentTime = 0;
+    vidRef.current.play();
+  }
+
+  function videoEnded(video) {
+    vidRef.current.load();
+    setPaused(false);
+    setPlaying(false);
+  }
+
+  function handleMouseMove(e) {
+    e.preventDefault();
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      setShowControls(false)
+    }, 1000);
+    setShowControls(true)
+  }
+
+  function handleTouchEnd(e) {
+    e.preventDefault()
+    if(playing || paused) {
+      console.log('touch')
+      setShowControls(true);
+      setTimeout(() => {
+        setShowControls(false)
+      }, 2000);
+    }
+  }
+
+  return (
+    <div className={"video" + (playing ? ' isPlaying' : '') + (paused ? ' isPaused': '') + (showControls ? ' showControls': '')} onMouseMove={handleMouseMove} onTouchEnd={handleTouchEnd}>
+      <video ref={vidRef} src={props.video} type="video/mp4" onEnded={stop} poster={props.poster} onEnded={videoEnded}>
+        Your browser does not support the video tag.
+      </video>
+      <div className="video-overlay">
+        <div className="video-controls">
+          <div className="video-controls__button video-controls__button--play" onClick={playToggle} onTouchEnd={playToggle}>
+            <div className="video-controls-button__play-icon"></div>
+            <div className="video-controls-button__pause-icon"></div>
+          </div>
+          <div className="video-controls__button video-controls__button--replay" onClick={replay} onTouchEnd={replay}></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VideoPlayer
