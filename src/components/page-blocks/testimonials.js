06f5692be6bc7da3a67eb1a8ea9dec8e64752dfb
Martin Bilek <martin.bilek@gmail.com>
2021-10-15 15:02:58 +0200

    update clients references

--- src/components/page-blocks/testimonials.js
+++ src/components/page-blocks/testimonials.js
@@ -4,8 +4,6 @@ import { FormattedMessage } from "react-intl"
 import Carousel from "react-multi-carousel"
 import "react-multi-carousel/lib/styles.css"
 
-import testimonialImg1 from "assets/img/testimonials/01-example-testimonial-image.jpg"
-
 const TestimonialsCarousel = ({ data }) => {
   const CustomButtonGroupAsArrows = ({ next, previous, ...rest }) => {
     const {
@@ -64,108 +62,89 @@ const TestimonialsCarousel = ({ data }) => {
         <div className="testimonial__item">
           <div className="testimonial__description">
             <div className="testimonial__description-text">
-              Jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz
-              nevydržel pouze pět století, on přežil i nástup elektronické sazby
-              v podstatě beze změny.
+              Karmen Pill je skvělé řešení, jak nejen vzdáleně dohlížet na 3D tiskárny a ovládat je. Pro naši školu je především skvělou pomůckou, jak lépe 3D tisk poskytnout žákům a udělat pro ně tuto novou technologii více zajímavou a ještě atraktivnější.
             </div>
           </div>
           <div className="testimonial__info">
-            <div className="testimonial__photo">
-              <img src={testimonialImg1} alt="" />
-            </div>
             <div className="testimonial__desc">
-              <span className="testimonial__title">Lorem ipsum</span>
-              <span className="testimonial__subtitle">Slovensko</span>
+              <span className="testimonial__title">Petr Novák</span>
+              <span className="testimonial__subtitle">Ředitel ZŠ MŠ Kladno</span>
             </div>
           </div>
         </div>
         <div className="testimonial__item">
           <div className="testimonial__description">
             <div className="testimonial__description-text">
-              Jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz
-              nevydržel pouze pět století, on přežil i nástup elektronické sazby
-              v podstatě beze změny.
+              Kamerky používám doma i v práci k plné spokojenosti. Mohu tisk kontrolovat a ovládat i z telefonu, při nějaké nehodě přerušit či úplně zastavit v noci si přisvítit. Rozhodně, k mé plné spokojenosti, to není klamavá reklama. V kombinaci s mou tiskárnou/tiskárnami je to velmi dobré řešení.
             </div>
           </div>
           <div className="testimonial__info">
-            <div className="testimonial__photo">
-              <img src={testimonialImg1} alt="" />
-            </div>
             <div className="testimonial__desc">
-              <span className="testimonial__title">Lorem ipsum</span>
-              <span className="testimonial__subtitle">Slovensko</span>
+              <span className="testimonial__title">Petr Musil</span>
+              <span className="testimonial__subtitle">LUKAS CZ spol s.r.o.</span>
             </div>
           </div>
         </div>
         <div className="testimonial__item">
           <div className="testimonial__description">
             <div className="testimonial__description-text">
-              Jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz
-              nevydržel pouze pět století, on přežil i nástup elektronické sazby
-              v podstatě beze změny.
+              Na Karmen se mi moc líbí i její skvěle zpracované UI, které mi hodně usnadňuje práci.
             </div>
           </div>
           <div className="testimonial__info">
-            <div className="testimonial__photo">
-              <img src={testimonialImg1} alt="" />
-            </div>
             <div className="testimonial__desc">
-              <span className="testimonial__title">Lorem ipsum</span>
-              <span className="testimonial__subtitle">Slovensko</span>
+              <span className="testimonial__title">JV</span>
+              <span className="testimonial__subtitle">AnyoneGo</span>
             </div>
           </div>
         </div>
         <div className="testimonial__item">
           <div className="testimonial__description">
             <div className="testimonial__description-text">
-              Jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz
-              nevydržel pouze pět století, on přežil i nástup elektronické sazby
-              v podstatě beze změny.
+              Už jsme si zvykli dávat gcody přes webové rozhraní. Hlavně u dlouhých tisků na to koukáme z domova a když je vidět, že něco nejde podle plánu, je super mít možnost to na dálku zastavit.
             </div>
           </div>
           <div className="testimonial__info">
-            <div className="testimonial__photo">
-              <img src={testimonialImg1} alt="" />
-            </div>
             <div className="testimonial__desc">
-              <span className="testimonial__title">Lorem ipsum</span>
-              <span className="testimonial__subtitle">Slovensko</span>
+              <span className="testimonial__title">MB</span>
+              <span className="testimonial__subtitle">IKEM</span>
             </div>
           </div>
         </div>
         <div className="testimonial__item">
           <div className="testimonial__description">
             <div className="testimonial__description-text">
-              Jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz
-              nevydržel pouze pět století, on přežil i nástup elektronické sazby
-              v podstatě beze změny.
+              Karmen je výborné řešení pro vzdálenou kontrolu a správu mého tisku a tiskárny. Tiskárnu mám umístěnou doma a nyní mohu kontrolovat tisk odkudkoliv. A v případě chybného tisku ho ihned zastavit.
             </div>
           </div>
           <div className="testimonial__info">
-            <div className="testimonial__photo">
-              <img src={testimonialImg1} alt="" />
-            </div>
             <div className="testimonial__desc">
-              <span className="testimonial__title">Lorem ipsum</span>
-              <span className="testimonial__subtitle">Slovensko</span>
+              <span className="testimonial__title">Jakub Šperl</span>
             </div>
           </div>
         </div>
         <div className="testimonial__item">
           <div className="testimonial__description">
             <div className="testimonial__description-text">
-              Jejich základě vytvořil speciální vzorovou knihu. Jeho odkaz
-              nevydržel pouze pět století, on přežil i nástup elektronické sazby
-              v podstatě beze změny.
+              Připojujeme se k iniciativě #TiskneCeleCesko. Ke kontrole a správě našich tiskáren využíváme software Karmen, který nám teď pomáhá víc než kdy jindy a snižuje náklady na správu tisku.
             </div>
           </div>
           <div className="testimonial__info">
-            <div className="testimonial__photo">
-              <img src={testimonialImg1} alt="" />
-            </div>
             <div className="testimonial__desc">
-              <span className="testimonial__title">Lorem ipsum</span>
-              <span className="testimonial__subtitle">Slovensko</span>
+              <span className="testimonial__title">David Frýbert</span>
+              <span className="testimonial__subtitle">Střední průmyslová škola Na Třebešíně</span>
+            </div>
+          </div>
+        </div>
+        <div className="testimonial__item">
+          <div className="testimonial__description">
+            <div className="testimonial__description-text">
+            Možnost tisk kdykoliv sledovat, když mám po ruce pouze mobil, případně ho zastavit je k nezaplacení, konečně beze strachu mohu tisknout, i když jsem v práci. Karmen Cloud je skvěle přehledný a jednoduchý na ovládání. Za mě dokonalý přístroj a dokonalý Cloud.
+            </div>
+          </div>
+          <div className="testimonial__info">
+            <div className="testimonial__desc">
+              <span className="testimonial__title">Michal Kanta</span>
             </div>
           </div>
         </div>
